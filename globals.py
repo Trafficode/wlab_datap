91ec7c2ddee31269497a903c3ff888d8033e85ff
kku <kku@kku-CELSIUS>
2020-02-25 21:39:38 +0100

    0.5

---
+++ globals.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+#   ------------------------------------------------------------------------- /
+#   wlab_datap: globals.py
+#   Created on: 29 sie 2019
+#   Author: Trafficode
+#   ------------------------------------------------------------------------- /
+
+class Globals(object):
+    
+    WLAB_VERSION     = "0.5"
+    
+    RELEASE_CONFIG_FILE  = '/home/wlab/weatherlab/config/config_wlabdatap.json'
+    
+    EXIT_CODE_REBOOT     = 10
+    EXIT_CODE_EXIT       = 11
+    EXIT_CODE_EXCEPTION  = 12
+    
+    CONFIG_IPC_DP_SERVER_PORT   = 2001
+    
+#   ------------------------------------------------------------------------- /
+#    end of file
+#   ------------------------------------------------------------------------- /
